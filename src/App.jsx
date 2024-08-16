27033bc22a70ffca7f4b0e478b832cb2125ddd3e
Yuliia shevchuk <shevchuuchkaaa@gmail.com>
2024-08-16 15:45:33 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
+import { useState } from 'react';
 import './App.scss';
+// import classNames from 'classnames/bind';
 
 export const goods = [
   'Dumplings',
@@ -14,30 +16,59 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [selectedGood, setGood] = useState('Jam');
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
-    </h1>
+  return (
+    <main className="section container">
+      {selectedGood ? (
+        <h1 className="title is-flex is-align-items-center">
+          {selectedGood} is selected
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setGood('')}
+          />
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      )}
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+      {goods.map(g => (
+        <table className="table">
+          <tbody>
+            <tr
+              data-cy="Good"
+              className={`${selectedGood === g && 'has-background-success-light'}`}
+            >
+              <td>
+                <button
+                  onClick={
+                    g === selectedGood ? () => setGood('') : () => setGood(g)
+                  }
+                  data-cy={`${g === selectedGood ? 'RemoveButton' : 'AddButton'}`}
+                  type="button"
+                  className={`button ${g === selectedGood && 'is-info'}`}
+                >
+                  {`${g === selectedGood ? '-' : '+'}`}
+                </button>
+              </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {g}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
+    </main>
+  );
+};
 
-        <tr data-cy="Good" className="has-background-success-light">
+/* <tr data-cy="Good" className="has-background-success-light">
           <td>
             <button
               data-cy="RemoveButton"
@@ -63,8 +94,4 @@ export const App = () => (
           <td data-cy="GoodTitle" className="is-vcentered">
             Garlic
           </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+        </tr> */
